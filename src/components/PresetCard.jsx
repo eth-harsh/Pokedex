cf711f601b5eb3dc7eba2f1bf8bbff6e17da54a5
Harsh <rajharsh52700@gmail.com>
2026-09-13 22:00:27 +0530

    Update PresetCard.jsx

--- src/components/PresetCard.jsx
+++ src/components/PresetCard.jsx
@@ -1,12 +1,12 @@
 const Data = [
-  { id: 1, name: "Bulbasaur", src: "/balbasaaur.png" },
-  { id: 6, name: "Charizard", src: "charigard.png" },
-  { id: 11, name: "Metapod", src: "/metapod.png" },
-  { id: 23, name: "Ekans", src: "ekans.png" },
-  { id: 24, name: "Arbok", src: "arbok.png" },
-  { id: 25, name: "Bulbasaur", src: "/balbasaaur.png" },
-  { id: 91, name: "Metapod", src: "/metapod.png" },
-  { id: 73, name: "Ekans", src: "ekans.png" },
+  { id: 1, name: "Bulbasaur", src: "/assets/balbasaaur.png" },
+  { id: 6, name: "Charizard", src: "/assets/charigard.png" },
+  { id: 11, name: "Metapod", src: "/assets/metapod.png" },
+  { id: 23, name: "Ekans", src: "/assets/ekans.png" },
+  { id: 24, name: "Arbok", src: "/assets/arbok.png" },
+  { id: 25, name: "Bulbasaur", src: "/assets/balbasaaur.png" },
+  { id: 91, name: "Metapod", src: "/assets/metapod.png" },
+  { id: 73, name: "Ekans", src: "/assets/ekans.png" },
 ];
 
 export default function PreSetCard() {
