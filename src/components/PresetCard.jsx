32e1ea6d621a0b9a37bfb82115e90a1f41bdf982
Harsh <rajharsh52700@gmail.com>
2026-09-13 22:01:45 +0530

    Change asset paths in PresetCard component
    Updated asset paths in Data array to use 'public/' prefix.

--- src/components/PresetCard.jsx
+++ src/components/PresetCard.jsx
@@ -1,12 +1,12 @@
 const Data = [
-  { id: 1, name: "Bulbasaur", src: "/assets/balbasaaur.png" },
-  { id: 6, name: "Charizard", src: "/assets/charigard.png" },
-  { id: 11, name: "Metapod", src: "/assets/metapod.png" },
-  { id: 23, name: "Ekans", src: "/assets/ekans.png" },
-  { id: 24, name: "Arbok", src: "/assets/arbok.png" },
-  { id: 25, name: "Bulbasaur", src: "/assets/balbasaaur.png" },
-  { id: 91, name: "Metapod", src: "/assets/metapod.png" },
-  { id: 73, name: "Ekans", src: "/assets/ekans.png" },
+  { id: 1, name: "Bulbasaur", src: "public/assets/balbasaaur.png" },
+  { id: 6, name: "Charizard", src: "public/assets/charigard.png" },
+  { id: 11, name: "Metapod", src: "public/assets/metapod.png" },
+  { id: 23, name: "Ekans", src: "public/assets/ekans.png" },
+  { id: 24, name: "Arbok", src: "public/assets/arbok.png" },
+  { id: 25, name: "Bulbasaur", src: "public/assets/balbasaaur.png" },
+  { id: 91, name: "Metapod", src: "public/assets/metapod.png" },
+  { id: 73, name: "Ekans", src: "public/assets/ekans.png" },
 ];
 
 export default function PreSetCard() {
