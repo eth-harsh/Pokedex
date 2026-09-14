ba04feadc6ce25ccd818071b69f7f61aad58f9cf
Harsh <rajharsh52700@gmail.com>
2026-09-14 22:51:21 +0530

    Update image sources to use BASE_URL

--- src/components/PresetCard.jsx
+++ src/components/PresetCard.jsx
@@ -1,12 +1,12 @@
 const Data = [
-  { id: 1, name: "Bulbasaur", src: "balbasaaur.png" },
-  { id: 6, name: "Charizard", src: "charigard.png" },
-  { id: 11, name: "Metapod", src: "metapod.png" },
-  { id: 23, name: "Ekans", src: "ekans.png" },
-  { id: 24, name: "Arbok", src: "arbok.png" },
-  { id: 25, name: "Bulbasaur", src: "balbasaaur.png" },
-  { id: 91, name: "Metapod", src: "metapod.png" },
-  { id: 73, name: "Ekans", src: "ekans.png" },
+  { id: 1, name: "Bulbasaur", src: `${import.meta.env.BASE_URL}balbasaaur.png` },
+  { id: 6, name: "Charizard", src: `${import.meta.env.BASE_URL}charigard.png` },
+  { id: 11, name: "Metapod", src: `${import.meta.env.BASE_URL}metapod.png` },
+  { id: 23, name: "Ekans", src: `${import.meta.env.BASE_URL}ekans.png` },
+  { id: 24, name: "Arbok", src: `${import.meta.env.BASE_URL}arbok.png` },
+  { id: 25, name: "Bulbasaur", src: `${import.meta.env.BASE_URL}balbasaaur.png` },
+  { id: 91, name: "Metapod", src: `${import.meta.env.BASE_URL}metapod.png` },
+  { id: 73, name: "Ekans", src: `${import.meta.env.BASE_URL}ekans.png` },
 ];
 
 export default function PreSetCard() {
